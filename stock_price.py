5553259e7b4c035794f79b0e8092d03a39d27351
sharkeatcandy <sc113943@yahoo.com.tw>
2021-07-01 20:35:58 +0800

    format

--- stock_price.py
+++ stock_price.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 import json
 import requests
+
+
 def check(number):
-    if number.isdigit() and len(number)==4:
+    if number.isdigit() and len(number) == 4:
         return number
     else:
         print('Please correct stock code')
         exit()
+
+
 def monitor(stock):
     stock = check(stock)
-    url = "http://mis.tse.com.tw/stock/api/getStock.jsp?ch=%s.tw&json=1&+="%(stock)
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
-    response = requests.get(url, headers=headers)
+    url = "http://mis.tse.com.tw/stock/api/getStock.jsp?ch=%s.tw&json=1&+=" % (stock)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
+    response = requests.get(url, headers=headers, verify=False)
     response_contents = json.loads(response.content)
     try:
         price = response_contents['msgArray'][0]['y']
@@ -19,7 +24,8 @@ def monitor(stock):
         print('Error stock code or api error，please try again')
     return price
 
+
 if __name__ == "__main__":
-    stock=input("股票代碼： ")
+    stock = input("股票代碼： ")
     price = monitor(stock)
-    print("收盤價：%7s"%price)
+    print("收盤價：%7s" % price)
