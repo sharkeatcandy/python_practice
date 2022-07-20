77ea90bc8979c18c862fd0a596429e6cca7b8b89
sharkeatcandy <sc113943@yahoo.com.tw>
2022-07-20 22:42:04 +0800

    add yt mp4 downloader

---
+++ download_youtube_mp4.py
@@ -0,0 +1,43 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    print(d['status'])
+    if d['status'] == 'finished':
+        print('下載完成')
+
+
+ydl_opts = {
+    'format': 'bestvideo[ext=mp4]+bestaudio',
+    'outtmpl': 'youtube_music/%(title)s',
+    'logger': MyLogger(),
+    'keep_video': True,
+    'progress_hooks': [my_hook]
+}
+if __name__ == "__main__":
+    while True:
+        try:
+            youtube_id = input('請輸入youtube網址，如果要結束請按Ctrl+C：\n')
+            if '=' in youtube_id:
+                youtube_id = youtube_id.split('=')[1]
+            if '&' in youtube_id:
+                youtube_id = youtube_id.split('&')[0]
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download(['https://www.youtube.com/watch?v=%s' % youtube_id])
+        except KeyboardInterrupt:
+            print(f"結束下載")
+            exit()
+        except Exception as e:
+            print(f"有東西出錯了：{e}")
