32c8bbb88933aa37bc3572229184fd7a8749e3c3
sharkeatcandy <sc113943@yahoo.com.tw>
2022-07-20 22:41:50 +0800

    update dcard search

--- dcard.py
+++ dcard.py
@@ -5,178 +5,200 @@ import datetime
 import os
 import sys
 import time
+
 token = ""
 
 # line API document: https://notify-bot.line.me/doc/en/
+
+
 def lineNotify(token, msg):
 
     url = "https://notify-api.line.me/api/notify"
-    headers = {
-        "Authorization": "Bearer " + token, 
-        "Content-Type" : "application/x-www-form-urlencoded"
-    }
-    
+    headers = {"Authorization": "Bearer " + token, "Content-Type": "application/x-www-form-urlencoded"}
+
     payload = {'message': msg}
-    r = requests.post(url, headers = headers, params = payload)
+    r = requests.post(url, headers=headers, params=payload)
     return r.status_code
 
+
 def monitor():
-	find_status = False
-	pic_status = False
-	txt = open('./test.txt', 'a', encoding = 'UTF-8')
-	url = 'https://www.dcard.tw/_api/forums/whysoserious/posts?limit=100&popular=false'
-	image_url = "附圖："
-	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
-	dcard = requests.get(url, headers=headers)
-	# os.system("echo 'd *' | mail")
-	if dcard.status_code == requests.codes.ok:
-		dcard_contents = json.loads(dcard.content)
-		for i in range(100):
-			try:
-				if(dcard_contents[i]['school']==u'銘傳大學' and dcard_contents[i]['department']==u'資訊工程學系'):
-					find_status = True
-					txt_read = open('./test.txt', 'r', encoding = 'UTF-8')
-					if "文章ID：%s"%(str(dcard_contents[i]['id'])) not in txt_read.read():
-						txt.write('\n')
-						txt.write('校名：')
-						txt.write(dcard_contents[i]['school'])
-						txt.write('\n')
-						txt.write('系名：')
-						txt.write(dcard_contents[i]['department'])
-						txt.write('\n')
-						txt.write('文章ID：')
-						txt.write(str(dcard_contents[i]['id']))
-						txt.write('\n')
-						txt.write('標題：')
-						txt.write(dcard_contents[i]['title'])
-						txt.write('\n')
-						txt.write('內文：')
-						txt.write(dcard_contents[i]['excerpt'])
-						txt.write('\n')
-						if dcard_contents[i]['mediaMeta'] != []:
-							pic_status = True
-							txt.write('附圖：')
-							txt.write('\n')
-							for item in dcard_contents[i]['mediaMeta']:
-								if str(item['url']).split('com/')[1].split('.')[0] not in image_url:
-									image_url += "\n%s"%(str(item['url']))
-									txt.write(item['url'])
-									txt.write('\n')
-						txt.write('現在時間：')
-						txt.write(str(datetime.datetime.now()))
-						txt.write('\n')
-						txt.write('\n')
-						if pic_status:
-							msg = "\n校名："+dcard_contents[i]['school']+"\n系名："+dcard_contents[i]['department']+"\n文章網址："+"http://dcard.tw/f/whysoserious/p/"+str(dcard_contents[i]['id'])+"\n標題："+dcard_contents[i]['title']+"\n內文："+dcard_contents[i]['excerpt']+"\n"+image_url
-						else:
-							msg = "\n校名："+dcard_contents[i]['school']+"\n系名："+dcard_contents[i]['department']+"\n文章網址："+"http://dcard.tw/f/whysoserious/p/"+str(dcard_contents[i]['id'])+"\n標題："+dcard_contents[i]['title']+"\n內文："+dcard_contents[i]['excerpt']
-						lineNotify(token, msg)
-					else:
-						txt.write('No new post '+str(datetime.datetime.now()))
-						txt.write('\n')
-				else:
-					pass
-			except:
-				pass
-	else:
-		txt.write('Get API failed at '+str(datetime.datetime.now()))
-		txt.write('\n')
+    find_status = False
+    pic_status = False
+    store_file = '/Users/shark_jhang/Documents/dcard.txt'
+    txt = open(store_file, 'a', encoding='UTF-8')
+    url = 'https://www.dcard.tw/service/api/v2/forums/whysoserious/posts?limit=100&popular=false'
+    image_url = "\n附圖："
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
+    }
+    dcard = requests.get(url, headers=headers)
+    key_words = ["銘傳資工", "小廢宅", "ㄒㄈㄓ", "03233"]
+    os.system("echo 'd *' | mail")
+    if dcard.status_code == requests.codes.ok:
+        dcard_contents = json.loads(dcard.content)
+        for i in range(100):
+            try:
+                if any(
+                    key_word in dcard_contents[i]['title'] + dcard_contents[i]['excerpt'] + dcard_contents[i]['school']
+                    for key_word in key_words
+                ):
+                    find_status = True
+                    txt_read = open(store_file, 'r', encoding='UTF-8')
+                    if "文章ID：%s" % (str(dcard_contents[i]['id'])) not in txt_read.read():
+                        txt.write('\n')
+                        txt.write('校名/卡稱：')
+                        txt.write(dcard_contents[i]['school'])
+                        txt.write('\n')
+                        if not dcard_contents[i]['anonymousDepartment']:
+                            txt.write('系名：')
+                            txt.write(dcard_contents[i]['department'])
+                            txt.write('\n')
+                        txt.write('文章ID：')
+                        txt.write(str(dcard_contents[i]['id']))
+                        txt.write('\n')
+                        txt.write('標題：')
+                        txt.write(dcard_contents[i]['title'])
+                        txt.write('\n')
+                        txt.write('內文：')
+                        txt.write(dcard_contents[i]['excerpt'])
+                        txt.write('\n')
+                        if dcard_contents[i]['mediaMeta'] != []:
+                            pic_status = True
+                            txt.write('附圖：')
+                            txt.write('\n')
+                            for item in dcard_contents[i]['mediaMeta']:
+                                if str(item['url']).split('com/')[1].split('.')[0] not in image_url:
+                                    image_url += "\n%s" % (str(item['url']))
+                                    txt.write(item['url'])
+                                    txt.write('\n')
+                        txt.write('現在時間：')
+                        txt.write(str(datetime.datetime.now()))
+                        txt.write('\n')
+                        txt.write('\n')
+                        msg = f"\n校名/卡稱：{dcard_contents[i]['school']}\n系名：{dcard_contents[i]['department'] if not dcard_contents[i]['anonymousDepartment'] else ''}\n文章網址：http://dcard.tw/f/whysoserious/p/{dcard_contents[i]['id']}\n標題：{dcard_contents[i]['title']}\n內文：{dcard_contents[i]['excerpt']}{image_url if pic_status else ''}"
+                        lineNotify(token, msg)
+                    else:
+                        txt.write('No new post ' + str(datetime.datetime.now()))
+                        txt.write('\n')
+                else:
+                    pass
+            except Exception as e:
+                print(e)
+    else:
+        txt.write('Get API failed at ' + str(datetime.datetime.now()))
+        txt.write('\n')
 
-	if not find_status:
-		txt.write('No post at '+str(datetime.datetime.now()))
-		txt.write('\n')
+    if not find_status:
+        txt.write('No post at ' + str(datetime.datetime.now()))
+        txt.write('\n')
 
-	txt.close()
+    txt.close()
+
 
 def search(number=100, from_id='', school='銘傳大學', department=''):
-	print('Search by school: ' + school + ' department: '+ department + ' in the last ' + str(number) + ' post' + ', from:' + from_id)
-	find_status = False
-	api_status = True
-	end_id = from_id
-	txt = open('./search.txt', 'w+', encoding = 'UTF-8')
-	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
-	n = 0
-	count = 0
-	while n < number:
-		if end_id:
-			url = 'https://www.dcard.tw/_api/forums/whysoserious/posts?limit=100&before=%s&popular=false'%(end_id)
-		else:
-			url = 'https://www.dcard.tw/_api/forums/whysoserious/posts?limit=100&popular=false'
-		dcard = requests.get(url, headers=headers)
-		if dcard.status_code == requests.codes.ok:
-			api_status = True
-			n += 100
-			dcard_contents = json.loads(dcard.content)
-			for i in range(100):
-				end_id = str(dcard_contents[i]['id'])
-				try:
-					if department:
-						if(dcard_contents[i]['school']=="%s"%(school) and dcard_contents[i]['department']=="%s"%(department)):
-							find_status = True
-							txt.write('校名：')
-							txt.write(dcard_contents[i]['school'])
-							txt.write('\n')
-							txt.write('系名：')
-							txt.write(dcard_contents[i]['department'])
-							txt.write('\n')
-							txt.write('文章網址：')
-							txt.write("http://dcard.tw/f/whysoserious/p/"+str(dcard_contents[i]['id']))
-							txt.write('\n')
-							txt.write('標題：')
-							txt.write(dcard_contents[i]['title'])
-							txt.write('\n')
-							txt.write('內文：')
-							txt.write(dcard_contents[i]['excerpt'])
-							txt.write('\n')
-							txt.write('\n')
-							count += 1
-					else:
-						if(dcard_contents[i]['school']=="%s"%(school)):
-							find_status = True
-							txt.write('校名：')
-							txt.write(dcard_contents[i]['school'])
-							txt.write('\n')
-							txt.write('文章網址：')
-							txt.write("http://dcard.tw/f/whysoserious/p/"+str(dcard_contents[i]['id']))
-							txt.write('\n')
-							txt.write('標題：')
-							txt.write(dcard_contents[i]['title'])
-							txt.write('\n')
-							txt.write('內文：')
-							txt.write(dcard_contents[i]['excerpt'])
-							txt.write('\n')
-							txt.write('\n')
-							count += 1
-				except:
-					pass
+    print(
+        'Search by school: '
+        + school
+        + ' department: '
+        + department
+        + ' in the last '
+        + str(number)
+        + ' post'
+        + ', from:'
+        + from_id
+    )
+    find_status = False
+    api_status = True
+    end_id = from_id
+    txt = open('./search.txt', 'w+', encoding='UTF-8')
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
+    }
+    n = 0
+    count = 0
+    while n < number:
+        if end_id:
+            url = 'https://www.dcard.tw/_api/forums/whysoserious/posts?limit=100&before=%s&popular=false' % (end_id)
+        else:
+            url = 'https://www.dcard.tw/_api/forums/whysoserious/posts?limit=100&popular=false'
+        dcard = requests.get(url, headers=headers)
+        if dcard.status_code == requests.codes.ok:
+            api_status = True
+            n += 100
+            dcard_contents = json.loads(dcard.content)
+            for i in range(100):
+                end_id = str(dcard_contents[i]['id'])
+                try:
+                    if department:
+                        if dcard_contents[i]['school'] == "%s" % (school) and dcard_contents[i][
+                            'department'
+                        ] == "%s" % (department):
+                            find_status = True
+                            txt.write('校名：')
+                            txt.write(dcard_contents[i]['school'])
+                            txt.write('\n')
+                            txt.write('系名：')
+                            txt.write(dcard_contents[i]['department'])
+                            txt.write('\n')
+                            txt.write('文章網址：')
+                            txt.write("http://dcard.tw/f/whysoserious/p/" + str(dcard_contents[i]['id']))
+                            txt.write('\n')
+                            txt.write('標題：')
+                            txt.write(dcard_contents[i]['title'])
+                            txt.write('\n')
+                            txt.write('內文：')
+                            txt.write(dcard_contents[i]['excerpt'])
+                            txt.write('\n')
+                            txt.write('\n')
+                            count += 1
+                    else:
+                        if dcard_contents[i]['school'] == "%s" % (school):
+                            find_status = True
+                            txt.write('校名：')
+                            txt.write(dcard_contents[i]['school'])
+                            txt.write('\n')
+                            txt.write('文章網址：')
+                            txt.write("http://dcard.tw/f/whysoserious/p/" + str(dcard_contents[i]['id']))
+                            txt.write('\n')
+                            txt.write('標題：')
+                            txt.write(dcard_contents[i]['title'])
+                            txt.write('\n')
+                            txt.write('內文：')
+                            txt.write(dcard_contents[i]['excerpt'])
+                            txt.write('\n')
+                            txt.write('\n')
+                            count += 1
+                except:
+                    pass
 
-		else:
-			txt.write(url)
-			txt.write(' ')
-			txt.write('Get API failed at '+str(datetime.datetime.now()))
-			txt.write('\n')
-			if api_status:
-				n -= 100
-			print('Because %d retrying'%dcard.status_code)
-			time.sleep(10)
-	if not find_status:
-		print('Not found any post at '+str(datetime.datetime.now()))
-	txt.close()
-	print("找到%d篇%s發的文章，詳細內容請看search.txt"%(count, school))
+        else:
+            txt.write(url)
+            txt.write(' ')
+            txt.write('Get API failed at ' + str(datetime.datetime.now()))
+            txt.write('\n')
+            if api_status:
+                n -= 100
+            print('Because %d retrying' % dcard.status_code)
+            time.sleep(10)
+    if not find_status:
+        print('Not found any post at ' + str(datetime.datetime.now()))
+    txt.close()
+    print("找到%d篇%s發的文章，詳細內容請看search.txt" % (count, school))
+
 
 if __name__ == "__main__":
-	if(len(sys.argv)==1):
-		monitor()
-	elif(sys.argv[1]=="search"):
-		if len(sys.argv)==3:
-			search(number=int(sys.argv[2]))
-		elif len(sys.argv)==4:
-			search(number=int(sys.argv[2]), school=sys.argv[3])
-		elif len(sys.argv)==5:
-			search(number=int(sys.argv[2]), school=sys.argv[3], department=sys.argv[4])
-		elif len(sys.argv)==6:
-			search(number=int(sys.argv[2]), from_id=sys.argv[3], school=sys.argv[4], department=sys.argv[5])
-		else:
-			search(number=100)
-	elif(sys.argv[1]=="help"):
-		print('Usage: python dcard.py search $number $from(optional) $school $department(optional)')
+    if len(sys.argv) == 1:
+        monitor()
+    elif sys.argv[1] == "search":
+        if len(sys.argv) == 3:
+            search(number=int(sys.argv[2]))
+        elif len(sys.argv) == 4:
+            search(number=int(sys.argv[2]), school=sys.argv[3])
+        elif len(sys.argv) == 5:
+            search(number=int(sys.argv[2]), school=sys.argv[3], department=sys.argv[4])
+        elif len(sys.argv) == 6:
+            search(number=int(sys.argv[2]), from_id=sys.argv[3], school=sys.argv[4], department=sys.argv[5])
+        else:
+            search(number=100)
+    elif sys.argv[1] == "help":
+        print('Usage: python dcard.py search $number $from(optional) $school $department(optional)')
--- dcard_data.py
+++ dcard_data.py
@@ -4,90 +4,93 @@ import requests
 import datetime
 import time
 
-def search(number=100, from_id='', school='銘傳大學', department=''):
-	print('Search by school: ' + school + ' department: '+ department + ' in the last ' + str(number) + ' post' + ', from:' + from_id)
-	find_status = False
-	api_status = False
-	end_id = from_id
-	txt = open('./search.txt', 'w+', encoding = 'UTF-8')
-	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
-	n = 0
-	count = 0
-	retry = 0
-	while n < number:
-		if end_id:
-			url = 'https://www.dcard.tw/_api/forums/whysoserious/posts?limit=100&before=%s&popular=false'%(end_id)
-		else:
-			url = 'https://www.dcard.tw/_api/forums/whysoserious/posts?limit=100&popular=false'
-		dcard = requests.get(url, headers=headers)
-		if dcard.status_code == requests.codes.ok:
-			retry = 0
-			api_status = True
-			n += 100
-			dcard_contents = json.loads(dcard.content)
-			for i in range(100):
-				end_id = str(dcard_contents[i]['id'])
-				try:
-					if department:
-						if(dcard_contents[i]['school']=="%s"%(school) and dcard_contents[i]['department']=="%s"%(department)):
-							find_status = True
-							txt.write('校名：')
-							txt.write(dcard_contents[i]['school'])
-							txt.write('\n')
-							txt.write('系名：')
-							txt.write(dcard_contents[i]['department'])
-							txt.write('\n')
-							txt.write('文章網址：')
-							txt.write("http://dcard.tw/f/whysoserious/p/"+str(dcard_contents[i]['id']))
-							txt.write('\n')
-							txt.write('標題：')
-							txt.write(dcard_contents[i]['title'])
-							txt.write('\n')
-							txt.write('內文：')
-							txt.write(dcard_contents[i]['excerpt'])
-							txt.write('\n')
-							txt.write('\n')
-							count += 1
-					else:
-						if(dcard_contents[i]['school']=="%s"%(school)):
-							find_status = True
-							txt.write('校名：')
-							txt.write(dcard_contents[i]['school'])
-							txt.write('\n')
-							txt.write('文章網址：')
-							txt.write("http://dcard.tw/f/whysoserious/p/"+str(dcard_contents[i]['id']))
-							txt.write('\n')
-							txt.write('標題：')
-							txt.write(dcard_contents[i]['title'])
-							txt.write('\n')
-							txt.write('內文：')
-							txt.write(dcard_contents[i]['excerpt'])
-							txt.write('\n')
-							txt.write('\n')
-							count += 1
-				except:
-					pass
 
-		else:
-			txt.write(url)
-			txt.write(' ')
-			txt.write('Get API failed at '+str(datetime.datetime.now()))
-			txt.write('\n')
-			if api_status:
-				api_status = False
-				n -= 100
-			retry += 1
-			print('因為 %d 正在重試第%d次'%(dcard.status_code, retry))
-			time.sleep(10)
-	if not find_status:
-		print('沒有找到任何PO文 '+str(datetime.datetime.now()))
-	txt.close()
-	print("找到%d篇%s發的文章，詳細內容請看search.txt"%(count, school))
-	time.sleep(10)
+def search(number=100, from_id='', school='', department=''):
+    print(f'Search by school: {school} department: {department} in the last {number} post ,from:  {from_id}')
+    find_status = False
+    api_status = False
+    end_id = from_id
+    txt = open('./search.txt', 'w+', encoding='UTF-8')
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
+    n = 0
+    count = 0
+    retry = 0
+    while n < number:
+        if end_id:
+            url = 'https://www.dcard.tw/service/api/v2/forums/whysoserious/posts?limit=100&before=%s&popular=false' % (
+                end_id)
+        else:
+            url = 'https://www.dcard.tw/service/api/v2/forums/whysoserious/posts?limit=100&popular=false'
+        dcard = requests.get(url, headers=headers)
+        if dcard.status_code == requests.codes.ok:
+            retry = 0
+            api_status = True
+            n += 100
+            dcard_contents = json.loads(dcard.content)
+            for i in range(100):
+                end_id = str(dcard_contents[i]['id'])
+                try:
+                    if department:
+                        if(dcard_contents[i]['school'] == "%s" % (school) and dcard_contents[i]['department'] == "%s" % (department)):
+                            find_status = True
+                            txt.write('校名：')
+                            txt.write(dcard_contents[i]['school'])
+                            txt.write('\n')
+                            txt.write('系名：')
+                            txt.write(dcard_contents[i]['department'])
+                            txt.write('\n')
+                            txt.write('文章網址：')
+                            txt.write("http://dcard.tw/f/whysoserious/p/"+str(dcard_contents[i]['id']))
+                            txt.write('\n')
+                            txt.write('標題：')
+                            txt.write(dcard_contents[i]['title'])
+                            txt.write('\n')
+                            txt.write('內文：')
+                            txt.write(dcard_contents[i]['excerpt'])
+                            txt.write('\n')
+                            txt.write('\n')
+                            count += 1
+                    else:
+                        if(dcard_contents[i]['school'] == "%s" % (school)):
+                            find_status = True
+                            txt.write('校名：')
+                            txt.write(dcard_contents[i]['school'])
+                            txt.write('\n')
+                            txt.write('文章網址：')
+                            txt.write("http://dcard.tw/f/whysoserious/p/"+str(dcard_contents[i]['id']))
+                            txt.write('\n')
+                            txt.write('標題：')
+                            txt.write(dcard_contents[i]['title'])
+                            txt.write('\n')
+                            txt.write('內文：')
+                            txt.write(dcard_contents[i]['excerpt'])
+                            txt.write('\n')
+                            txt.write('\n')
+                            count += 1
+                except:
+                    pass
+
+        else:
+            txt.write(url)
+            txt.write(' ')
+            txt.write(f'Get API failed at {str(datetime.datetime.now())}')
+            txt.write('\n')
+            if api_status:
+                api_status = False
+                n -= 100
+            retry += 1
+            print(f'因為 {dcard.status_code} 正在重試第{retry}次')
+            time.sleep(10)
+    if not find_status:
+        print(f'沒有找到任何PO文 {str(datetime.datetime.now())}')
+    txt.close()
+    print(f"找到{count}篇{school}{department}發的文章，詳細內容請看search.txt")
+
 
 if __name__ == "__main__":
-	number = input('請輸入要在最近幾篇文章內搜尋：\n')
-	from_id = input('請輸入要從第幾篇開始往前搜尋(如果從最近開始搜尋直接按enterl就好)：\n')
-	school = input('請輸入要搜尋的校名(卡稱也可以)：\n')
-	department = input('請輸入要搜尋的系名(如果沒有要搜尋系名或是搜尋卡稱就直接按enter)：\n')
-	search(number=int(number), from_id=int(from_id) if from_id else from_id, school=school, department=department)
+    number = input('請輸入要在最近幾篇文章內搜尋：\n')
+    from_id = input('請輸入要從第幾篇開始往前搜尋(如果從最近開始搜尋直接按enter就好)：\n')
+    school = input('請輸入要搜尋的校名(卡稱也可以)：\n')
+    department = input('請輸入要搜尋的系名(如果沒有要搜尋系名或是搜尋卡稱就直接按enter)：\n')
+    search(number=int(number), from_id=int(from_id) if from_id else from_id, school=school, department=department)
